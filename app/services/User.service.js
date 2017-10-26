c1925779c07971fa8d392f351b697c41f7a68170
louis.pujol <louis.pujol@blackbaud.com>
2017-10-26 12:35:15 -0500

    update louis and melissa's facebook ids

--- app/services/User.service.js
+++ app/services/User.service.js
@@ -9,10 +9,10 @@ export class UserService {
 
     private populateDb() {
         //TODO: Update IDs with actual FB Ids
-        this.usersDb.push(new User('Louis Pujol', 1));
+        this.usersDb.push(new User('Louis Pujol', 1314119188601872));
         this.usersDb.push(new User('Lauren Hunter', 746452268710522));
         this.usersDb.push(new User('Kevin Fung', 2));
         this.usersDb.push(new User('Kevin Hudson', 3));
-        this.usersDb.push(new User('Melissa Feather', 4));
+        this.usersDb.push(new User('Melissa Feather', 683210195161618));
     }
 }
