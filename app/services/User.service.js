d7023d0418ab50cfff061ade3d98a4120924236e
louis.pujol <louis.pujol@blackbaud.com>
2017-10-26 12:25:11 -0500

    populate database with users

--- app/services/User.service.js
+++ app/services/User.service.js
@@ -2,4 +2,16 @@ import { User } from "../shared/user/User";
 
 export class UserService {
     public usersDb: User[] = [];
+
+    constructor() {
+        this.populateDb();
+    }
+
+    private populateDb() {
+        this.usersDb.push(new User('Louis Pujol', 1));
+        this.usersDb.push(new User('Lauren Hunter', 746452268710522));
+        this.usersDb.push(new User('Kevin Fung', 2));
+        this.usersDb.push(new User('Kevin Hudson', 3));
+        this.usersDb.push(new User('Melissa Feather', 4));
+    }
 }
