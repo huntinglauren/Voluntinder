c785a77567fe99a59f33f46b25794dd476db62f1
louis.pujol <louis.pujol@blackbaud.com>
2017-10-26 15:37:42 -0500

    user service must be in the login method

--- app/components/FacebookLogin.js
+++ app/components/FacebookLogin.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { UserService } from '../services/User.service';
-import { User } from '../shared/user/User';
 
 export default class FacebookLogin extends Component {
-    userService = new UserService();
-
     constructor(props) {
         super(props);
         this.state = { username: ''};
     }
 
     login = async function logIn() {
+        let userService = new UserService();
         const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('342498269493972', {
             permissions: ['public_profile'],
         });
@@ -20,8 +18,9 @@ export default class FacebookLogin extends Component {
         if (type === 'success') {
             // Get the user's name using Facebook's Graph API
             const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-            console.log(response);
-            let user: User = this.userService.getUserByFacebookId(response.id);
+            let responseJSON = await response.json();
+            console.log(responseJSON.id);
+            let user = userService.getUserByFacebookId(Number(responseJSON.id));
         }
     };
 
