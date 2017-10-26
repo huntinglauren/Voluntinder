4caedf4b0a44df27bf4797a3018fce38a4942324
louis.pujol <louis.pujol@blackbaud.com>
2017-10-26 13:20:51 -0500

    add method for getting user by id

--- app/services/User.service.js
+++ app/services/User.service.js
@@ -1,18 +1,28 @@
 import { User } from "../shared/user/User";
 
 export class UserService {
-    public usersDb: User[] = [];
+    usersDB: User[];
 
     constructor() {
+        this.usersDB = [];
         this.populateDb();
     }
 
-    private populateDb() {
+    getUserById(targetId: number) {
+        for (let user of this.usersDB) {
+            if (user.id === targetId) {
+                return user;
+            }
+        }
+        return undefined;
+    }
+
+    populateDb() {
         //TODO: Update IDs with actual FB Ids
-        this.usersDb.push(new User('Louis Pujol', 1314119188601872));
-        this.usersDb.push(new User('Lauren Hunter', 746452268710522));
-        this.usersDb.push(new User('Kevin Fung', 2));
-        this.usersDb.push(new User('Kevin Hudson', 3));
-        this.usersDb.push(new User('Melissa Feather', 683210195161618));
+        this.usersDB.push(new User('Louis Pujol', 1314119188601872));
+        this.usersDB.push(new User('Lauren Hunter', 746452268710522));
+        this.usersDB.push(new User('Kevin Fung', 2));
+        this.usersDB.push(new User('Kevin Hudson', 501938162));
+        this.usersDB.push(new User('Melissa Feather', 683210195161618));
     }
 }
